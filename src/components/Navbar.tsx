d54d3da3704c099e829511cac7fb8ba22c6e26fd
Dennis Campos <dennisjr89@gmail.com>
2023-12-30 10:13:29 -0500

    add dark mode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Button } from './ui/button';
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
+import { ModeToggle } from './Toggle';
 
 export async function Navbar() {
   const cookieStore = cookies();
@@ -12,20 +13,23 @@ export async function Navbar() {
   } = await supabase.auth.getUser();
 
   return (
-    <header>
+    <header className="border-b border-gray-500/50 bg-card">
       <div className="flex justify-between items-center p-2">
         <Button variant="ghost" size="sm" asChild>
           <Link href="/">Bookmark Vibes</Link>
         </Button>
-        {user ? (
-          <form action="/auth/signout" method="post">
-            <Button size="sm">Logout</Button>
-          </form>
-        ) : (
-          <Button size="sm" asChild>
-            <Link href="/login">Login</Link>
-          </Button>
-        )}
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          {user ? (
+            <form action="/auth/signout" method="post">
+              <Button size="sm">Logout</Button>
+            </form>
+          ) : (
+            <Button size="sm" asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
+        </div>
       </div>
     </header>
   );
---
+++ src/components/SidebarNav.tsx
@@ -0,0 +1,14 @@
+export const SidebarNav = () => {
+  return (
+    <nav className="bg-card border-r border-gray-500/50 w-[200px] h-screen">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
