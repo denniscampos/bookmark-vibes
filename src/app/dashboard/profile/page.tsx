5ab755304a790cc00209e288ba16b497902b4a78
Dennis Campos <dennisjr89@gmail.com>
2024-04-19 20:44:39 -0400

    base start for uploading profile pics

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,10 @@
+import { ProfileUpload } from './ProfileUpload';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Profile page.</h1>
+      <ProfileUpload />
+    </div>
+  );
+}
